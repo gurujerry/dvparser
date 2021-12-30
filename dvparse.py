b8f140dd0a277c9785be6573c048cf738e950a5b
stratusjerry <icandevops@gmail.com>
2021-12-30 15:37:13 -0500

    pep linting

--- dvparse.py
+++ dvparse.py
@@ -8,16 +8,16 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 ''' A re-write of big list using element[x].parent.text '''
 ## Variables to edit:
-#Boolean to turn on testing mode
-justTest = False ; maxTest = 10
+# Boolean to turn on testing mode
+justTest = False; maxTest = 10
 useProxy = True
-#Compares to previous CSV to reduce page lookups
-compareCSV = False ; oldCSV = '/path/to/oldfile.csv'
-#Remove low ratings
-filterRating = True ; minRating = 4.2
-outputCSV = True ; newCSV = '/path/to/new.csv' ; filteredCSV = '/path/to/newFiltered.csv'
-sleepMin = 2; sleepMax = 5  #1,2
-#Old Regex to remove low ratings: ^"(3.*|4\.[0-1].*)\n  OR Better: ^"([1-3]|(4("|\.[0-1]))).*\n
+# Compares to previous CSV to reduce page lookups
+compareCSV = False; oldCSV = '/path/to/oldfile.csv'
+# Remove low ratings
+filterRating = True; minRating = 4.2
+outputCSV = True; newCSV = '/path/to/new.csv'; filteredCSV = '/path/to/newFiltered.csv'
+sleepMin = 2; sleepMax = 5
+# Old Regex to remove low ratings: ^"(3.*|4\.[0-1].*)\n  OR Better: ^"([1-3]|(4("|\.[0-1]))).*\n
 
 proxies = {
   "http": "http://12.69.91.226:80"
@@ -31,10 +31,10 @@ def parseCSV(filepath):
     return csvList
 
 
-def writeCSV(writeFile, rowList, filterRating = False):
+def writeCSV(writeFile, rowList, filterRating=False):
     with open(writeFile, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        writer.writerow(['Rating','Beer Name','Price','Abv','URL','Style','Ratings'])
+        writer.writerow(['Rating', 'Beer Name', 'Price', 'Abv', 'URL', 'Style', 'Ratings'])
         if filterRating:
             for row in rowList:
                 try:
@@ -49,7 +49,7 @@ def writeCSV(writeFile, rowList, filterRating = False):
 
 
 #url = 'http://ifconfig.me' #Gets IP
-def get_data_from_ut(url, useProxy = False):
+def get_data_from_ut(url, useProxy=False):
     try:
         user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0'
         user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
@@ -68,11 +68,11 @@ def get_data_from_ut(url, useProxy = False):
 
 def parseLink(linkDescription):
     linkDescription = linkDescription.strip('\n')
-    ## Is the regex end of line and group 2 below necessary/wrong?
-    ## TODO: Add new case like "$1.00 - 1 left"
-    matchLimit = re.match( r'(.*)( - limit.*?) .*', linkDescription, re.M|re.I) #remove ' - limit.*
-    matchLeft = re.match( r'(.*)( - [0-9]+ left.*?).*', linkDescription, re.M|re.I) #remove ' - left.*
-    matchIce = re.match( r'(.*)( - .*ice.?pack.*?) .*', linkDescription, re.M|re.I) #was picky
+    # Is the regex end of line and group 2 below necessary/wrong?
+    # TODO: Add new case like "$1.00 - 1 left"
+    matchLimit = re.match(r'(.*)( - limit.*?) .*', linkDescription, re.M | re.I)  # remove ' - limit.*
+    matchLeft = re.match(r'(.*)( - [0-9]+ left.*?).*', linkDescription, re.M | re.I)  # remove ' - left.*
+    matchIce = re.match(r'(.*)( - .*ice.?pack.*?) .*', linkDescription, re.M | re.I)  # was picky
     if matchLimit:
         print("Found regex Limit")
         linkDescription = matchLimit.group(1)
@@ -97,10 +97,10 @@ if compareCSV or outputCSV:
 if compareCSV:
     oldCSVList = parseCSV(oldCSV)
 
-## Get all the email links and their parents, written this way because parent.link 
-##   can be duplicated and parent.string can be 3 different edge cases
+# Get all email links and their parents, written this way because parent.link
+#   can be duplicated and parent.string can be 3 different edge cases
 html_doc = BeautifulSoup(gmail, 'html.parser')
-allLinks = html_doc.findAll('a') #len(allLinks) 308
+allLinks = html_doc.findAll('a')  # len(allLinks) 308
 allElems = len(allLinks) - 1
 linkTextList = []; columnList = []; total = 0; beername = ""
 foundInCSV = foundNAInCSV = foundPriceInCSV = 0
@@ -114,8 +114,8 @@ for link in allLinks:
     if justTest and total > maxTest:
         print("Stopping Processing")
         break
-    ## TODO: pass this to Function parse
-    if link.parent.text in linkTextList: #Check for dupe links?
+    # TODO: pass this to Function parse
+    if link.parent.text in linkTextList:  # Check for dupe links?
         print(f"     Got a dupe link {link.parent.text}")
         continue
     else:
@@ -125,8 +125,8 @@ for link in allLinks:
     if checkGoodLink == "Bad Link":
         print("     Bad Link, skipping")
         continue
-    # Making spaces optional (?) because DV sucks at formatting
-    beerEmailDescription = re.match( r'(.*)( ?- ?)(.*)', checkGoodLink, re.M|re.I)
+    # Spaces optional (?) because DV sucks at formatting
+    beerEmailDescription = re.match(r'(.*)( ?- ?)(.*)', checkGoodLink, re.M | re.I)
     beername = beerEmailDescription.group(1)
     formattedBeername = beername.strip()
     price = beerEmailDescription.group(3)
@@ -157,14 +157,14 @@ for link in allLinks:
                     itemList = [csvRating, csvName, formattedPrice, csvABV, csvURL, csvStyle, csvRatings]
                 columnList.append(itemList)
                 csvAddedRecord = True
-        if csvAddedRecord: #TODO: there's probably a better way continue an outer nested for loop
+        if csvAddedRecord:  # TODO: probably a better way continue an outer nested for loop
             continue
             #for key, value in elem.items():
             #    print(f'key: {key} value: {value}')
-    time.sleep(random.uniform(sleepMin, sleepMax)) ## Throttle between requests, uniform vs randint for floating numbers
+    time.sleep(random.uniform(sleepMin, sleepMax))  # Throttle between requests, uniform vs randint for floating numbers
     #resp = get_data_from_ut(link['href'])  # not itemList[0]
     if useProxy:
-        resp = get_data_from_ut(link['href'], useProxy = True)
+        resp = get_data_from_ut(link['href'], useProxy=True)
     else:
         resp = get_data_from_ut(link['href'])
     #? Replace with resp.raise_for_status()
@@ -172,7 +172,7 @@ for link in allLinks:
         print("No 200 error")
         break
     resp_doc = BeautifulSoup(resp.text, 'html.parser')
-    rating = resp_doc.find("span", {"class": "num"}).text #'(4.67)'
+    rating = resp_doc.find("span", {"class": "num"}).text  # '(4.67)'
     formattedRating = rating.strip('()')
     abv = resp_doc.find("p", {"class": "abv"}).text
     formattedAbv = abv.strip('\nABV ')
@@ -196,7 +196,7 @@ for c in columnList:
         except ValueError:
             pass
             #print(f"    Can't convert to float {c[0]}")
-        ## I hated the ugly regex below and replaced with float comparrison above
+        # I hated the ugly regex below and replaced with float comparison above
         #if re.match( r'^([0-3]|(4($|\.[0-1]))).*$', c[0] ):
         #    continue
     if totalNewCSVRows == 0:
@@ -207,4 +207,4 @@ for c in columnList:
 if outputCSV:
     writeCSV(newCSV, columnList)
     if filterRating:
-        writeCSV(filteredCSV, columnList, filterRating = True)
+        writeCSV(filteredCSV, columnList, filterRating=True)
